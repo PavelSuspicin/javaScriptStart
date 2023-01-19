da9f42bba64b9074e44b79428a0e5d421f9f97cd
Pavel Suspicin <pavelsuspicin@gmail.com>
2023-01-19 11:37:43 +0300

    x

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-function checkObj(obj, checkProp) {
-  // Only change code below this line
-  return obj.hasOwnProperty(checkProp) ? `${checkProp}` : false;
-  // Only change code above this line
+function f(n) {
+  return n
 }
 
-console.log(checkObj({ gift: "pony", pet: "kitten", bed: "sleigh" }, "gift"));
+console.log(f(10))
